711f1b49b522c97f2cf88056668194e2b6755153
Chris Lopez <clopenz@outlook.com>
2024-07-05 23:31:35 -0700

    final final

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,6 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie parser middleware
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-	res.send('API is running...');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -39,6 +35,10 @@ const __dirname = path.resolve(); // Set __dirname to current directory
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 if (process.env.NODE_ENV === 'production') {
+	const __dirname = path.resolve();
+
+	app.use('/uploads', express.static('/var/data/uploads'));
+
 	// Set static folder
 	app.use(express.static(path.join(__dirname, '/frontend/build')));
 
@@ -47,6 +47,9 @@ if (process.env.NODE_ENV === 'production') {
 		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 	);
 } else {
+	const __dirname = path.resolve();
+	app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
 	app.get('/', (req, res) => {
 		res.send('API is running...');
 	});
